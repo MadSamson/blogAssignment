6a2fc4c600d517efde0ea3ad58658cdf3f7ceb09
MadSamson <m.mahdi.samadi@gmail.com>
2022-03-10 19:45:55 +0100

    follow/unfollow toggle button

--- controller/usersprofile.js
+++ controller/usersprofile.js
@@ -1,23 +1,25 @@
 const {User} = require('../models/user')
 const {Tweet} = require('../models/tweet')
+
+
 const usersProfile = async(req, res) => {
+    let status = 'follow'
+    if(req.user){
+        req.user.following.includes(req.params.id) ? status = 'unfollow' : status = 'follow'
+    }
     const userId = await req.params.id
-    const user = await User.findOne({_id:userId})
+    const postOwner = await User.findOne({_id:userId})
     const userstweets = await Tweet.find({author: userId}).populate('author').sort({publishingDate:-1})
     res.render('usersprofile.ejs', {
-        user,
-        userstweets
+        postOwner,
+        userstweets,
+        status 
     })
 }
 const usersProfile_post = async(req, res)=>{
     try {
-        // if(!req.user) return res.redirect('/login')
         const usersIdToFollow = req.params.id
         if(req.user._id==usersIdToFollow) return res.redirect('/')
-        if(req.user.following.indexOf(usersIdToFollow)!==-1) {
-            req.flash('error', 'already following')
-            return res.redirect(`/user/${usersIdToFollow}`)
-        }
         await User.findByIdAndUpdate(req.user.id, { $push: { following: usersIdToFollow } })
         await User.findByIdAndUpdate(usersIdToFollow, { $push: { followers: req.user._id } })
         res.redirect(`/user/${usersIdToFollow}`)
@@ -28,7 +30,6 @@ const usersProfile_post = async(req, res)=>{
 
 const usersProfile_delete = async(req, res)=>{
     try {
-        // if(!req.user) return res.redirect('/login')
         const usersIdToFollow = req.params.id
         if(req.user._id==usersIdToFollow) return res.redirect('/')
         await User.findByIdAndUpdate(req.user.id, { $pull: { following: usersIdToFollow } })
